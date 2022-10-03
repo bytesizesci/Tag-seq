eb1d3bf3865c0f57c7752dd51a1cdafb8904af49
Kristen James <krijames@ucdavis.edu>
2022-10-03 12:03:21 -0700

    Update to KJs environment, update to more recent releases

--- star_index_mm10.sh
+++ star_index_mm10.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 #
 #SBATCH --job-name=star_index
-#SBATCH --workdir /share/lasallelab/Ben/PEBBLES/tag-seq
+#SBATCH --workdir /home/kristen1/22projects/DOProgenitor
 #SBATCH --ntasks=30 # Number of cores/threads
 #SBATCH --mem=64000 # Ram in Mb
-#SBATCH --partition=production 
+#SBATCH --partition=high
 #SBATCH --time=0-01:00:00
 
 ##########################################################################################
-# Author: Ben Laufer
+# Original Author: Ben Laufer
 # Email: blaufer@ucdavis.edu 
 # Modified from: https://leonjessen.wordpress.com/2014/12/01/how-do-i-create-star-indices-using-the-newest-grch38-version-of-the-human-genome/
+#
+# Adapted by: Kristen James
+# Email: krijames@ucdavis.edu
+# Lab: Bennett Lab, USDA-ARS WHNRC
+# Date: 10/03/2022
 ##########################################################################################
 
 ###################
@@ -43,7 +48,9 @@ mkdir -p data/GRCm38/sequence
 
 cd data/GRCm38/sequence/
 
-wget ftp://ftp.ensembl.org/pub/release-95/fasta/mus_musculus/dna/Mus_musculus.GRCm38.dna.primary_assembly.fa.gz
+#wget ftp://ftp.ensembl.org/pub/release-95/fasta/mus_musculus/dna/Mus_musculus.GRCm38.dna.primary_assembly.fa.gz
+# the latest version that still uses GRCm38; corresponds to the qtl2 files
+wget ftp://ftp.ensembl.org/pub/release-102/fasta/mus_musculus/dna/Mus_musculus.GRCm38.dna.primary_assembly.fa.gz
 
 gunzip Mus_musculus.GRCm38.dna.primary_assembly.fa.gz
 
@@ -57,9 +64,9 @@ mkdir -p data/GRCm38/annotation
 
 cd data/GRCm38/annotation/
 
-wget ftp://ftp.ensembl.org/pub/release-95/gtf/mus_musculus/Mus_musculus.GRCm38.95.gtf.gz
-
-gunzip Mus_musculus.GRCm38.95.gtf.gz
+#wget ftp://ftp.ensembl.org/pub/release-95/gtf/mus_musculus/Mus_musculus.GRCm38.95.gtf.gz
+wget ftp://ftp.ensembl.org/pub/release-102/gtf/mus_musculus/Mus_musculus.GRCm38.102.gtf.gz
+gunzip Mus_musculus.GRCm38.102.gtf.gz
 
 cd ../../../
 
@@ -75,7 +82,7 @@ call="STAR \
 --runMode genomeGenerate \
 --genomeDir data/GRCm38/star_100/ \
 --genomeFastaFiles data/GRCm38/sequence/Mus_musculus.GRCm38.dna.primary_assembly.fa \
---sjdbGTFfile data/GRCm38/annotation/Mus_musculus.GRCm38.95.gtf \
+--sjdbGTFfile data/GRCm38/annotation/Mus_musculus.GRCm38.102.gtf \
 --sjdbOverhang 99"
 
 echo $call
